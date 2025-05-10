01713a16e14c5d35e4ae0ca0ff5162ec5866549a
radio-satellites <VE3SVF@gmail.com>
2025-05-10 10:52:28 -0400

    Add 3d models

--- add_kicad.sh
+++ add_kicad.sh
@@ -1,3 +1,3 @@
 read -p "Enter LCSC ID: " id
 
-easyeda2kicad --symbol --footprint --lcsc_id="$id" --output ~/Documents/OS-41/libs
+easyeda2kicad --symbol --footprint --3d --lcsc_id="$id" --output ~/Documents/OS-41/libs
